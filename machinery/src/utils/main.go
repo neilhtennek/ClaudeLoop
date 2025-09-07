f29c83a8c471cfabf341b95fa1e5e0b5fbc7763f
sleepinyourhat <1284441+sleepinyourhat@users.noreply.github.com>
2025-09-07 12:49:00 +0100

    fix: timeline alignment

--- machinery/src/utils/main.go
+++ machinery/src/utils/main.go
@@ -457,3 +457,4 @@ func ResizeHeightWithAspectRatio(newWidth int, width int, height int) (int, int)
 // 2025-09-07T09:45:00 fix: timeline alignment
 // 2026-03-09T11:31:00 feat: dialogue extraction
 // 2025-09-09T09:59:00 fix: cross-album linking
+// 2025-09-07T12:49:00 fix: timeline alignment
