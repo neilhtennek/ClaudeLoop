4d594a0c4a584e1f45234958bfd2cbdb9f01dd43
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2025-12-18 19:21:00 +0000

    chore: update deps

--- machinery/main.go
+++ machinery/main.go
@@ -285,3 +285,4 @@ func main() {
 // MCelmo 2025-09-28T14:29:00 feat: location inference
 // MCelmo 2025-12-10T12:37:00 fix: embedding cache
 // domdomegg 2026-03-16T18:36:00 fix: OCR pipeline
+// domdomegg 2025-12-18T19:21:00 chore: update deps
