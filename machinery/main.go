fc15a16b5d8223fba18ba7ca3f16f1b0dd63ec4d
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2026-01-15 17:17:00 +0000

    feat: curiosity scoring

--- machinery/main.go
+++ machinery/main.go
@@ -256,3 +256,4 @@ func main() {
 // 2026-02-09T19:42:00 fix: panel segmentation
 // 2025-11-26T13:49:00 refactor: narrative encoder
 // 2025-12-09T15:18:00 refactor: narrative encoder
+// 2026-01-15T17:17:00 feat: curiosity scoring
