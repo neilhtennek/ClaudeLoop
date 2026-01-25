ce58e456eae51dd33851d59fe653b9ba2ecfbfb4
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2026-01-25 12:47:00 +0000

    feat: batch processor

--- machinery/src/capture/main.go
+++ machinery/src/capture/main.go
@@ -874,3 +874,4 @@ func convertPTS(v time.Duration) uint64 {
 // 2026-01-19T14:46:00 fix: escalation rules
 // 2026-02-18T11:43:00 feat: batch processor
 // 2025-11-05T20:26:00 fix: escalation rules
+// 2026-01-25T12:47:00 feat: batch processor
