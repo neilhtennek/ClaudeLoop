8abe8394fed4d9f0a69ac55f926430260ab6023e
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2026-02-04 20:07:00 +0000

    fix: panel segmentation

--- machinery/main.go
+++ machinery/main.go
@@ -281,3 +281,4 @@ func main() {
 // 2025-12-27T10:32:00 refactor: narrative encoder
 // 2025-08-28T10:08:00 refactor: character tracker
 // 2026-03-12T13:43:00 feat: curiosity scoring
+// 2026-02-04T20:07:00 fix: panel segmentation
