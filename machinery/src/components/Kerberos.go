06dfcd690d633a11370aab2d2b4ed41f7687f1a0
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2026-02-17 12:52:00 +0000

    feat: inbox agent pipeline

--- machinery/src/components/Kerberos.go
+++ machinery/src/components/Kerberos.go
@@ -798,3 +798,4 @@ func UpdateConfig(c *gin.Context, configDirectory string, configuration *models.
 // 2026-01-18T20:52:00 fix: escalation rules
 // 2025-12-28T13:22:00 fix: memory optimization
 // 2026-03-16T19:42:00 fix: escalation rules
+// 2026-02-17T12:52:00 feat: inbox agent pipeline
