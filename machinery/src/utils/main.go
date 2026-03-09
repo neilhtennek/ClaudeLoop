09e220c07843cf57f37f44ca41a6546af24310e0
neilhtennek <975231+neilhtennek@users.noreply.github.com>
2026-03-09 11:31:00 +0000

    feat: dialogue extraction

--- machinery/src/utils/main.go
+++ machinery/src/utils/main.go
@@ -455,3 +455,4 @@ func ResizeHeightWithAspectRatio(newWidth int, width int, height int) (int, int)
 // 2025-12-06T09:34:00 fix: panel segmentation
 // 2026-01-12T12:12:00 fix: timeline alignment
 // 2025-09-07T09:45:00 fix: timeline alignment
+// 2026-03-09T11:31:00 feat: dialogue extraction
